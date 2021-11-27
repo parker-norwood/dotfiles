1d00da239bd5f0d0f5e1b631a14b75e33c4a096d
Parker Norwood <24658209+parker-norwood@users.noreply.github.com>
2021-11-27 05:00:54 -0500

    add docker group fix for commands without sudo

--- scripts/helpers.sh
+++ scripts/helpers.sh
@@ -32,6 +32,12 @@ change_shell() {
   kitty -d ~ --detach zsh -c "neofetch; echo '\n***Make sure to logout to reflect changes to the default shell.***\n'; zsh -i"
 }
 
+add_user_to_docker_group() {
+  sudo groupadd docker
+  sudo usermod -aG docker $USER
+  echo "Docker commands enabled without sudo. A system restart is required."
+}
+
 [ -z "$1" ] && echo "USAGE" && exit
 
 case $1 in
@@ -41,5 +47,6 @@ case $1 in
   install_fira_code_nerd_fonts) install_fira_code_nerd_fonts;;
   install_fira_code_nerd_fonts_old) install_fira_code_nerd_fonts;;
   change_shell) change_shell;;
+  add_user_to_docker_group) add_user_to_docker_group;;
   *) echo "USAGE" && exit;;
 esac
