ade4e99e9cff5ea44fc8940e7d7304534e726fd5
Parker Norwood <p.norwood4294@gmail.com>
2021-11-27 04:56:42 -0500

    add git ppa

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -56,6 +56,8 @@ add_sources() {
   # Google Chrome
   wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
   sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+  # git
+  sudo add-apt-repository -y ppa:git-core/ppa
 }
 
 add_sources
