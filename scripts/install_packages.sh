ff0f90d237626cbb26a515217118e6798dd02413
Parker Norwood <24658209+parker-norwood@users.noreply.github.com>
2022-04-28 21:33:06 -0400

    update to work with ubuntu 22.04

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -6,41 +6,45 @@ apt_packages=(
   dconf-editor # simple configuration storage system - graphical editor
   fzf # general-purpose command-line fuzzy finder
   gh # GitHub’s official command line tool.
-  gir1.2-gconf-2.0 # GNOME configuration database system (GObject-introspection)
+  # gir1.2-gconf-2.0 # GNOME configuration database system (GObject-introspection)
   git # fast, scalable, distributed revision control system
   git-lfs # git Large File Support
-  gnome-shell-extensions # extensions to extend functionality of GNOME Shell
+  # gnome-shell-extensions # extensions to extend functionality of GNOME Shell
   gnome-tweaks # tool to adjust advanced configuration settings for GNOME
   google-chrome-stable # the web browser from Google
   htop # interactive processes viewer
   imagemagick # image manipulation programs -- binaries
   kitty # fast, featureful, GPU based terminal emulator
-  miktex # MiKTeX: a scalable TeX distribution
+  # miktex # MiKTeX: a scalable TeX distribution # TODO: uncomment once MiKTeX has a jammy universe repo
   neofetch # shows linux system information with distribution logo
   protonvpn # ProtonVPN metapackage
   python3-nautilus # python binding for Nautilus components (Python 3 version)
   python3-pip # python package installer
   vim # Vi IMproved - enhanced vi editor
   vim-airline # Lean & mean status/tabline for vim that's light as air
+  virtualbox # x86 virtualization solution
   zsh # shell with lots of features
   # net-tools # deprecated, use iproute2 utilities (ip/ss commands) instead
   # dnsutils # deprecated, use dig instead
 )
 
 apt_packages_remove=(
-  firefox # Safe and easy web browser from Mozilla
   gnome-terminal # GNOME terminal emulator application
 )
 
 snaps=(
   bpytop # A Python-based resource monitor for your terminal
-  caprine # unofficial and privacy-focused facebook messenger desktop app
+  # caprine # unofficial and privacy-focused facebook messenger desktop app
   discord # all-in-one voice and text chat for gamers
   docker # docker container runtime
   postman # API development environment
   signal-desktop # Signal Private Messenger for Windows, Mac, and Linux
   spotify # music for everyone
   vlc # the ultimate media player
+)
+
+snaps_remove=(
+  firefox # Safe and easy web browser from Mozilla
 )
 
 classic_snaps=(
@@ -51,10 +55,15 @@ classic_snaps=(
 
 python3_packages=()
 
+python3_user_packages=(
+  nautilus-open-any-terminal
+)
+
 add_sources() {
   # MiKTeX
-  sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D6BC243565B2087BC3F897C9277A7293F59E4889
-  echo "deb [arch=amd64] http://miktex.org/download/ubuntu focal universe" | sudo tee /etc/apt/sources.list.d/miktex.list
+  # TODO: uncomment once MiKTeX has a jammy universe repo
+  # sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D6BC243565B2087BC3F897C9277A7293F59E4889
+  # echo "deb [arch=amd64] http://miktex.org/download/ubuntu jammy universe" | sudo tee /etc/apt/sources.list.d/miktex.list
   # Google Chrome
   wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
   sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
@@ -84,6 +93,10 @@ for snap in ${classic_snaps[@]}; do
   sudo snap install $snap --classic
 done
 
+(( ${#snaps_remove[@]} )) && sudo snap remove ${snaps_remove[@]}
+
 (( ${#python3_packages[@]} )) && sudo pip3 install ${python3_packages[@]}
+
+(( ${#python3_user_packages[@]} )) && pip3 install --user ${python3_user_packages[@]}
 
 exit 0
