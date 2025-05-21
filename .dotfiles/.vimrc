cde2945f28ed073241f73a67a690e02d5becc10f
Parker Norwood <24658209+parker-norwood@users.noreply.github.com>
2025-05-21 02:31:22 -0400

    switch to chezmoi

--- .dotfiles/.vimrc
+++
@@ -1,6 +0,0 @@
-packadd! dracula
-syntax enable
-colorscheme dracula
-set termguicolors
-let g:airline_theme='dracula'
-let g:airline_powerline_fonts = 1
---
+++ dotfiles/run_once_after_setup.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+change_shell() {
+  chsh -s $(which zsh)
+}
+
+change_terminal() {
+  sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator $(which ghostty) 100
+  sudo update-alternatives --set x-terminal-emulator $(which ghostty)
+}
+
+install_firacode_nerd_font() {
+  mkdir -p ~/.local/share/fonts
+  wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/FiraCode.tar.xz
+  tar -xvf FiraCode.tar.xz -C ~/.local/share/fonts --wildcards *.ttf
+  fc-cache -fv
+  rm FiraCode.tar.xz
+}
+
+add_user_to_docker_group() {
+  sudo groupadd docker
+  sudo usermod -aG docker $USER
+  newgrp docker
+  sudo snap disable docker
+  sudo snap enable docker
+}
+
+setup_git() {
+  gh auth login
+  gh auth setup-git
+  read -p "git config --global user.name " git_user_name
+  git config --global user.name $git_user_name
+  read -p "git config --global user.email " git_user_email
+  git config --global user.name $git_user_email
+}
+
+dconf_load() {
+  dconf load / < $CHEZMOI_SOURCE_DIR/dconf.ini
+}
+
+change_shell
+change_terminal
+install_firacode_nerd_font
+add_user_to_docker_group
+setup_git
+dconf_load
